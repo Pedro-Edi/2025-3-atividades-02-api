600722973d19d6968688b84f30e34553a3b0f7c3
Pedro Edi <pedro.edi12345678g@gmail.com>
2025-10-09 09:48:42 -0300

    Criação do projeto

---
+++ src/main.ts
@@ -0,0 +1,24 @@
+import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  // Habilitar CORS
+  app.enableCors({
+    origin: true,
+    credentials: true,
+  });
+
+  // Habilitar validação global
+  app.useGlobalPipes(new ValidationPipe({
+    transform: true,
+    whitelist: true,
+    forbidNonWhitelisted: true,
+  }));
+
+  await app.listen(3000);
+  console.log('API rodando em http://localhost:3000');
+}
+bootstrap();
